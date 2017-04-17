6c37fe31b4ef63c073cc3bf989bc9d06ba27332b
mhcrnl <mhcrnl@gmail.com>
2017-04-17 19:26:14 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <gtk/gtk.h>
+
+static void helloWorld (GtkWidget *wid, GtkWidget *win) {
+    GtkWidget *dialog = NULL;
+
+    dialog = gtk_message_dialog_new (GTK_WINDOW (win), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_CLOSE, "Hello World!");
+    gtk_window_set_position (GTK_WINDOW (dialog), GTK_WIN_POS_CENTER);
+    gtk_dialog_run (GTK_DIALOG (dialog));
+    gtk_widget_destroy (dialog);
+}
+
+int main (int argc, char *argv[]) {
+    //GtkWidget *button = NULL;
+    GtkWidget *win = NULL;
+    GtkWidget *vbox = NULL;
+    /*
+        Create menu
+    */
+    GtkWidget *menubar;
+    GtkWidget *fileMenu;
+    GtkWidget *fileMi;
+    GtkWidget *quitMi;
+    /*
+        Initialize GTK+
+    */
+    g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, (GLogFunc) gtk_false, NULL);
+    gtk_init (&argc, &argv);
+    g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, g_log_default_handler, NULL);
+
+    /* Create the main window */
+    win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_container_set_border_width (GTK_CONTAINER (win), 8);
+    gtk_window_set_title (GTK_WINDOW (win), "MhcrnlTextEditor");
+    gtk_window_set_position (GTK_WINDOW (win), GTK_WIN_POS_CENTER);
+    gtk_window_set_default_size(GTK_WINDOW(win), 800, 600);
+    gtk_widget_realize (win);
+    g_signal_connect (win, "destroy", gtk_main_quit, NULL);
+
+    /* Create a vertical box with buttons */
+    vbox = gtk_vbox_new (FALSE, 0);
+    gtk_container_add (GTK_CONTAINER (win), vbox);
+
+    menubar = gtk_menu_bar_new();
+    fileMenu = gtk_menu_new();
+
+    fileMi = gtk_menu_item_new_with_label("File");
+    quitMi = gtk_menu_item_new_with_label("Quit");
+
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(fileMi), fileMenu);
+    gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), quitMi);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menubar), fileMi);
+
+    //button = gtk_button_new_from_stock (GTK_STOCK_DIALOG_INFO);
+    //g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (helloWorld), (gpointer) win);
+    gtk_box_pack_start (GTK_BOX (vbox), menubar, FALSE, FALSE, 0);
+
+    //button = gtk_button_new_from_stock (GTK_STOCK_CLOSE);
+    g_signal_connect (G_OBJECT(quitMi), "activate",
+                      G_CALLBACK(gtk_main_quit), NULL);
+    //gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
+
+    /* Enter the main loop */
+    gtk_widget_show_all (win);
+    gtk_main ();
+    return 0;
+}
